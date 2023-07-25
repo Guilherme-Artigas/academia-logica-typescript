36989e21132b15a9a4e8c8b87c9f0f66bb661b6d
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-07-25 08:42:57 -0300

    feat: implementação da função position

---
+++ src/codewars/level-eight/position.ts
@@ -0,0 +1,34 @@
+export function position(alphabet: string): string {
+  const listLetters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
+
+  const index = listLetters.findIndex((e) => e === alphabet);
+
+  return `Position of alphabet: ${index + 1}`;
+}
